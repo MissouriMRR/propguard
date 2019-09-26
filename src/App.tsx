710350e28cde9fa44f4d9cc8f803cb912521ae1d
Tom Dong <translucentleaf@protonmail.com>
2019-09-26 18:34:34 -0500

    Created react-typescript boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>High tech React app NOW WITH TYPESCRIPT!!!!!</h1>
+      <h1>Please create your own branch before working with this project!</h1>
+      <h1>And please put your components in the src/components folder.</h1>
+    </div>
+  );
+}
+
+export default App;
