f6a2d833db2754ab6a24bd1dee282ff697a35783
Tom Dong <translucentleaf@protonmail.com>
2019-09-26 18:11:41 -0500

    Pushed react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TestComponent from './components/TestComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Isn't this the most high-tech React project you've ever seen?</h1>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TestComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TestComponent() {
+  return(
+    <h1>This is a test component</h1>
+  );
+}
