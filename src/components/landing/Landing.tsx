22b968eb44bddf2c0d2bfb788014008757ba72f6
Henri Evjen <henrievjen@gmail.com>
2019-09-29 22:37:59 -0500

    updated meta info

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Landing.css';
+import terminal from './landing-images/terminal.gif';
 
 const Landing: React.FC = () => {
     return (
@@ -24,7 +25,7 @@ const Landing: React.FC = () => {
                     <div className="container h-100">
                         <div className="row align-items-center h-100">
                             <div className="terminal-container col-lg-12">
-                                <img className="terminal" src="https://i.github-camo.com/d770ff03b2e7f22079b485c24f9fe0963fd8acde/68747470733a2f2f6769746875622e636f6d2f706c6174666f726d696f2f706c6174666f726d696f2d61746f6d2d6964652d7465726d696e616c2f7261772f6d61737465722f7265736f75726365732f64656d6f2e676966" alt="Terminal"></img>
+                                <img className="terminal" src={terminal} alt="Terminal"></img>
                             </div>
                         </div>
                     </div>
