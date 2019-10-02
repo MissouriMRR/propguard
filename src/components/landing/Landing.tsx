e102a803b04af8f30999e16b14c606e911ff2be3
Henri Evjen <henrievjen@gmail.com>
2019-10-01 22:26:26 -0500

    landing page disappears when click 'Start Tutorial'

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Landing.css';
 import terminal from './landing-images/terminal.gif';
 
 const Landing: React.FC = () => {
+    const [display, setDisplay] = useState('block');
+
+    const startTutorial = () => {
+        setDisplay('none');
+    }
+
     return (
-        <div className="Landing">
+        <div className="Landing" style={{display: display}}>
             <div className="row h-75">
                 <div className="col-xl-6">
                     <div className="container h-100">
@@ -15,7 +21,7 @@ const Landing: React.FC = () => {
                                         Propguard
                                     </h1>
                                     <small>A webapp that teaches people how to use Dronekit and Python to start and launch the drones</small> <br></br>
-                                    <button className="btn btn-primary col-6 mt-5">Start Tutorial</button>
+                                    <button className="btn btn-primary col-6 mt-5" onClick={startTutorial}>Start Tutorial</button>
                                     <div className="mt-4"><a href="#">Wiki</a></div>
                                 </div>
                             </div>
