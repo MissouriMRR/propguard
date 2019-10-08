66608b1dd633cff4868756f4d4637a5d49659d46
henrievjen <henrievjen@gmail.com>
2019-10-08 18:32:47 -0500

    removed terminal

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -12,30 +12,15 @@ const Landing: React.FC = () => {
 
     return (
         <div className="Landing" style={{display: display}}>
-            <div className="row h-75">
-                <div className="col-xl-6">
-                    <div className="container h-100">
-                        <div className="row align-items-center h-100">
-                            <div className="col-8 mx-auto text-center">
-                                <div className="jumbotron text-center mt-5 mt-xl-0">
-                                    <h1 className="title anim-typewriter">
-                                        Propguard
-                                    </h1>
-                                    <small>A webapp that teaches people how to use Dronekit and Python to start and launch the drones</small> <br></br>
-                                    <Link to="/tutorial"><button className="btn btn-primary col-6 mt-5" onClick={startTutorial}>Start Tutorial</button></Link>
-                                    <div className="mt-4"><a href="#">Wiki</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-6">
-                    <div className="container h-100">
-                        <div className="row align-items-center h-100">
-                            <div className="terminal-container col-lg-12">
-                                <img className="terminal pb-5" src={terminal} alt="Terminal"></img>
-                            </div>
-                        </div>
+            <div className="container h-100 text-center">
+                <div className="row align-items-center h-100">
+                    <div className="col-lg-6 col-md-8 col-10 offset-lg-3 offset-md-2 offset-1 jumbotron text-center mt-5 mt-xl-0">
+                        <h1 className="title anim-typewriter">
+                            Propguard
+                        </h1>
+                        <small>A webapp that teaches people how to use Dronekit and Python to start and launch the drones</small> <br></br>
+                        <Link to="/tutorial"><button className="btn btn-primary col-6 mt-5" onClick={startTutorial}>Start Tutorial</button></Link>
+                        <div className="mt-4"><a href="#">Wiki</a></div>
                     </div>
                 </div>
             </div>
