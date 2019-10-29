dcf8352657473e535fc00ac8126327ab484d08a0
henrievjen <henrievjen@gmail.com>
2019-10-29 18:23:44 -0500

    added semantic elements and moved styled components outside components

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import styled, { AnyStyledComponent } from "styled-components";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Landing } from "./components/landing/Landing";
 
-const App: React.FC = (): JSX.Element => {
-  const Main: AnyStyledComponent = styled.div`
-    height: 100vh;
-    overflow-y: hidden;
-  `;
+const Main: AnyStyledComponent = styled.main`
+  height: 100vh;
+  overflow-y: hidden;
+`;
 
+const App: React.FC = (): JSX.Element => {
   return (
     <Router>
       <Main>
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -2,74 +2,74 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled, { AnyStyledComponent } from "styled-components";
 
+const Land: AnyStyledComponent = styled.div`
+  height: 100%;
+  overflow-x: hidden;
+  background-color: #454c60;
+`;
+
+const Main: AnyStyledComponent = styled.main`
+  height: 350px;
+  color: white;
+
+  @media screen and (max-width: 575px) {
+    .jumbotron {
+      padding-top: 60px;
+    }
+  }
+`;
+
+const Title: AnyStyledComponent = styled.h1`
+  position: relative;
+  font-size: 40px;
+  width: 16em;
+  height: 16%;
+  margin: 0 auto;
+  border-right: 2px solid rgba(255, 255, 255, 0);
+  overflow: hidden;
+  transform: translateY(-50%);
+  font-weight: 400;
+  animation: typewriter 1s steps(9) 1s 1 normal both,
+    blinkTextCursor 1s steps(11) infinite normal;
+
+  @keyframes typewriter {
+    from {
+      width: 0;
+    }
+    to {
+      width: 190px;
+    }
+  }
+
+  @keyframes blinkTextCursor {
+    0% {
+      border-right-color: rgba(255, 255, 255, 1);
+    }
+    25% {
+      border-right-color: rgba(255, 255, 255, 1);
+    }
+    50% {
+      border-right-color: rgba(255, 255, 255, 1);
+    }
+    75% {
+      border-right-color: transparent;
+    }
+    100% {
+      border-right-color: transparent;
+    }
+  }
+`;
+
+const Subtext: AnyStyledComponent = styled.aside`
+  font-size: 16px;
+`;
+
+const Button: AnyStyledComponent = styled.button`
+  background-color: #afeb80;
+  color: white;
+`;
+
 const Landing: React.FC = (): JSX.Element => {
-  const Land: AnyStyledComponent = styled.div`
-    height: 100%;
-    overflow-x: hidden;
-    background-color: #454c60;
-  `;
-
-  const Main: AnyStyledComponent = styled.div`
-    height: 350px;
-    color: white;
-
-    @media screen and (max-width: 575px) {
-      .jumbotron {
-        padding-top: 60px;
-      }
-    }
-  `;
-
-  const Title: AnyStyledComponent = styled.div`
-    position: relative;
-    font-size: 40px;
-    width: 16em;
-    height: 16%;
-    margin: 0 auto;
-    border-right: 2px solid rgba(255, 255, 255, 0);
-    overflow: hidden;
-    transform: translateY(-50%);
-    font-weight: 400;
-    animation: typewriter 1s steps(9) 1s 1 normal both,
-      blinkTextCursor 1s steps(11) infinite normal;
-
-    @keyframes typewriter {
-      from {
-        width: 0;
-      }
-      to {
-        width: 190px;
-      }
-    }
-
-    @keyframes blinkTextCursor {
-      0% {
-        border-right-color: rgba(255, 255, 255, 1);
-      }
-      25% {
-        border-right-color: rgba(255, 255, 255, 1);
-      }
-      50% {
-        border-right-color: rgba(255, 255, 255, 1);
-      }
-      75% {
-        border-right-color: transparent;
-      }
-      100% {
-        border-right-color: transparent;
-      }
-    }
-  `;
-
-  const Subtext: AnyStyledComponent = styled.span`
-    font-size: 16px;
-  `;
-
-  const Button: AnyStyledComponent = styled.button`
-    background-color: #afeb80;
-    color: white;
-  `;
-
   return (
     <Land>
       <div className="container h-100 text-center">
