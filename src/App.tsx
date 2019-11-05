99522010f4d10707b955ef5daf957d81549f7d58
Tom Dong <translucentleaf@protonmail.com>
2019-11-05 17:36:14 -0600

    Fixed import issue with TutorialApp component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled, { AnyStyledComponent } from "styled-components";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { TutorialApp } from "./components/tutorial-app/TutorialApp";
 import { Landing } from "./components/landing/Landing";
-import { TutorialApp } from "components/tutorial-app/TutorialApp";
 
 const App: React.FC = (): JSX.Element => {
   const Main: AnyStyledComponent = styled.div`
