94c99aa4d2163313ffbeeb1cd25346b9281f1ce0
henrievjen <henrievjen@gmail.com>
2019-11-19 17:31:38 -0600

    Removing more files from the old Propguard stack

--- src/components/output/Output.tsx
+++ src/components/output/Output.tsx
@@ -3,12 +3,17 @@ import styled, { AnyStyledComponent } from "styled-components";
 import turtle from "./images/turtle.png";
 
 const Main: AnyStyledComponent = styled.div`
+  display: inline-block;
   margin-top: 20px;
   width: 75%;
   height: 90%;
   border-radius: 5px;
   background-color: white;
   text-align: center;
+
+  @media only screen and (max-width: 800px) {
+    text-align: left;
+  }
 `;
 
 const Turtle: AnyStyledComponent = styled.img`
@@ -17,20 +22,32 @@ const Turtle: AnyStyledComponent = styled.img`
   width: 150px;
   height: auto;
   margin-top: 25px;
+  display: inline;
 
   @media only screen and (max-width: 800px) {
-    position: absolute;
-    left: 30px;
+    margin-top: 0;
+    padding: 10px;
+    display: inline-block;
   }
 `;
 
-const Data: AnyStyledComponent = styled.section`
+const Data: AnyStyledComponent = styled.div`
+  position: relative;
   margin-top: 50px;
   text-align: center;
   font-weight: bold;
+  display: inline;
+
+  @media only screen and (max-width: 800px) {
+    margin-top: 0px;
+    display: inline-block;
+    padding-top: 20px;
+    padding-left: 20px;
+  }
 `;
 
 const Armed: AnyStyledComponent = styled.h1`
+  position: relative;
   font-size: 30px;
   font-weight: normal;
   margin-bottom: 10px;
@@ -51,12 +68,16 @@ const ResetBtn: AnyStyledComponent = styled.button`
   margin-left: -57.5px;
   outline: none;
   border: none;
+  display: inline;
 
   :hover {
     background-color: #a5a5a5;
   }
 
   @media only screen and (max-width: 800px) {
+    position: relative;
+    top: 0;
+    bottom: 0;
   }
 `;
 
