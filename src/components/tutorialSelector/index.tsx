c839eae4ac64342c8b17d1737db1a8b1a39e3ab0
Henri Evjen <henrievjen@gmail.com>
2020-03-30 10:09:12 -0500

    moved styled components outside component

--- src/components/tutorialSelector/index.tsx
+++ src/components/tutorialSelector/index.tsx
@@ -6,6 +6,49 @@ import styled, { AnyStyledComponent } from "styled-components";
 import { useStaticQuery, graphql } from "gatsby";
 import { Tutorial } from "../interfaces";
 
+interface SelectorProps {
+  display: string;
+}
+
+const TutorialMain: AnyStyledComponent = styled.div`
+  background-color: #262626;
+  color: #fff;
+  height: 100%;
+  width: 100%;
+  display: ${(props: SelectorProps): string => props.display};
+`;
+
+const SingleTutorial: AnyStyledComponent = styled.div`
+  color: #fff;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Title: AnyStyledComponent = styled.h1`
+  font-size: 24px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 100;
+  padding-left: 20px;
+  padding-top: 20px;
+`;
+
+const Description: AnyStyledComponent = styled.div`
+  font-size: 14px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 100;
+  padding-left: 20px;
+  padding-bottom: 20px;
+`;
+
+const ThinLine: AnyStyledComponent = styled.hr`
+  height: 1px;
+  width: 100%;
+  background: #727272;
+  border: none;
+`;
+
 const TutorialSelector: React.FC = (): JSX.Element => {
   const [, setTutorialStep] = useGlobal("tutorialStep");
   const [, setTutorialName] = useGlobal("tutorialName");
@@ -31,49 +74,6 @@ const TutorialSelector: React.FC = (): JSX.Element => {
       }
     }
   `);
-
-  interface SelectorProps {
-    display: string;
-  }
-
-  const TutorialMain: AnyStyledComponent = styled.div`
-    background-color: #262626;
-    color: #fff;
-    height: 100%;
-    width: 100%;
-    display: ${(props: SelectorProps): string => props.display};
-  `;
-
-  const SingleTutorial: AnyStyledComponent = styled.div`
-    color: #fff;
-
-    &:hover {
-      cursor: pointer;
-    }
-  `;
-
-  const Title: AnyStyledComponent = styled.h1`
-    font-size: 24px;
-    font-family: "Open Sans", sans-serif;
-    font-weight: 100;
-    padding-left: 20px;
-    padding-top: 20px;
-  `;
-
-  const Description: AnyStyledComponent = styled.div`
-    font-size: 14px;
-    font-family: "Open Sans", sans-serif;
-    font-weight: 100;
-    padding-left: 20px;
-    padding-bottom: 20px;
-  `;
-
-  const ThinLine: AnyStyledComponent = styled.hr`
-    height: 1px;
-    width: 100%;
-    background: #727272;
-    border: none;
-  `;
 
   return (
     <TutorialMain display={selectorDisplay}>
