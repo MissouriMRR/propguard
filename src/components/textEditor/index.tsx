68bedf8d27b89c056504bf4cafff7ca2455bb1b4
Henri Evjen <henrievjen@gmail.com>
2020-04-28 13:52:29 -0500

    styling fixes

--- src/components/textEditor/index.tsx
+++ src/components/textEditor/index.tsx
@@ -4,29 +4,32 @@
 import React, { useState } from "react";
 import styled, { AnyStyledComponent } from "styled-components";
 import AceEditor from "react-ace";
+import { background, grey } from "../../constants";
+import { Button } from "./button";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/ext-language_tools";
-import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-kr_theme";
 
 const TerminalWrapper: AnyStyledComponent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  width: 100%;
 `;
 
-const Button: AnyStyledComponent = styled.button`
-  background-color: #90c577;
-  border-radius: 5px;
-  border-style: none;
-  height: 43px;
-  width: 98px;
-  font-size: 18px;
-  margin: 10px 0 2rem 0;
-
-  @media only screen and (max-width: 800px) {
-    bottom: 60px;
-  }
+const TerminalHeader: AnyStyledComponent = styled.div`
+  height: 4rem;
+  width: 100%;
+  padding: 1rem 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid ${grey};
+  border-left: none;
+  border-right: none;
+  border-top: none;
 `;
 
 const TextEditor: React.FC = (): JSX.Element => {
@@ -44,15 +47,21 @@ const TextEditor: React.FC = (): JSX.Element => {
 
   return (
     <TerminalWrapper>
+      <TerminalHeader>
+        <Button submitFunction={handleSubmit} text="Hint" />
+        <Button submitFunction={handleSubmit} text="Run" />
+      </TerminalHeader>
       <AceEditor
         style={{
           position: "relative",
-          marginTop: 20,
-          height: "80vh",
-          width: "30vw"
+          marginTop: "1vh",
+          height: "90vh",
+          width: "100%",
+          backgroundColor: background
         }}
+        fontSize="18px"
         mode="python"
-        theme="monokai"
+        theme="kr_theme"
         placeholder="Type in anything you want to your heart’s content. Text wrapping is included too!"
         value={userInput}
         onChange={(value: string): void => {
@@ -65,7 +74,6 @@ const TextEditor: React.FC = (): JSX.Element => {
           tabSize: 2
         }}
       />
-      <Button onClick={handleSubmit}>Run</Button>
     </TerminalWrapper>
   );
 };
