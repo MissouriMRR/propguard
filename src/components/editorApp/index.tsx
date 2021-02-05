0a6e58296e4decde53a8911b8064cadde1660bc2
Tom Dong <contact@tomdong.io>
2021-02-04 20:55:34 -0600

    Added html skeleton for editor splash screen

---
+++ src/components/editorApp/index.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import styled, { AnyStyledComponent } from "styled-components";
+
+import { Navbar } from "../navbar";
+
+import { background, codeColor, grey } from "../../constants";
+
+const StyledEditorContainer: AnyStyledComponent = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-items: stretch;
+  background: ${background};
+  overflow: auto;
+`;
+
+const StyledWelcomeContainer: AnyStyledComponent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    font-size: 24px;
+    margin: 0;
+  }
+`;
+
+const StyledWelcomeHeader: AnyStyledComponent = styled.header`
+  padding: 1rem;
+  border: 1px solid ${grey};
+  border-left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWelcomeSection: AnyStyledComponent = styled.main`
+  padding: 4rem 4rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: stretch;
+
+  h1 {
+    font-size: 48px;
+    margin-bottom: 1rem;
+  }
+
+  p {
+  }
+`;
+
+const StyledWelcomeInfo: AnyStyledComponent = styled.div`
+  flex: 2;
+`;
+
+const StyledCard: AnyStyledComponent = styled.div`
+  max-width: 800px;
+  margin: 2rem 4rem 2rem 0;
+  padding: 1rem;
+  background: ${codeColor};
+  border-radius: 2px;
+`;
+
+const StyledWelcomeSidebar: AnyStyledComponent = styled.div`
+  flex: 1;
+`;
+
+const EditorApp: React.FC = () => {
+  return (
+    <StyledEditorContainer>
+      <Navbar />
+      <StyledWelcomeContainer>
+        <StyledWelcomeHeader>
+          <h2>Tutorial Editor</h2>
+        </StyledWelcomeHeader>
+        <StyledWelcomeSection>
+          <StyledWelcomeInfo>
+            <h1>Propguard Editor</h1>
+            <p>Pass the torch to the next generation by writing tutorials.</p>
+            <StyledCard>
+              <h2>Modify</h2>
+              <ul>
+                <li>Select an existing tutorial</li>
+                <li>Or upload your own tutorial file (JSON)</li>
+              </ul>
+            </StyledCard>
+            <StyledCard>
+              <h2>Create</h2>
+              <ul>
+                <li>Create a brand new tutorial from scratch</li>
+              </ul>
+            </StyledCard>
+          </StyledWelcomeInfo>
+          <StyledWelcomeSidebar>
+            <h2>Need to update propguard?</h2>
+            <p>
+              <a
+                href="https://github.com/MissouriMRR/propguard/blob/master/contributing-tutorials.md"
+                target="blank"
+              >
+                Click here
+              </a>{" "}
+              for instructions on updating the live Propguard instance or
+              contact one of the maintainers below.
+            </p>
+            <ul>
+              <li>Tommy Dong</li>
+              <li>Henri Evjen</li>
+              <li>Jack Novatny</li>
+            </ul>
+          </StyledWelcomeSidebar>
+        </StyledWelcomeSection>
+      </StyledWelcomeContainer>
+    </StyledEditorContainer>
+  );
+};
+
+export { EditorApp };
