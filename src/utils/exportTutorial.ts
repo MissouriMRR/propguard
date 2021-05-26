eec93225d1623f0ffe4437546446544e9b4b9405
Tom Dong <contact@tomdong.io>
2021-05-25 21:41:24 -0500

    JSON export should export all parts of the tutorial editor now

--- src/utils/exportTutorial.ts
+++ src/utils/exportTutorial.ts
@@ -1,5 +1,12 @@
 import { EditorStep } from "../types/editorTypes";
 
+// Converts the stringified answer key into an array of strings,
+// with each element representing a line of code
+const convertAnswer = (answer: string): string[] => {
+  const newAnswer = answer.split("\n").filter(string => string.length !== 0);
+  return newAnswer;
+};
+
 /**
  * Used to take the current state of the tutorial Editor and export it
  * into a JSON file that can be used to import a tutorial for the user.
@@ -18,7 +25,10 @@ export const exportTutorial = async (
       title: step.stepTitle,
       hint: step.stepHint,
       content: step.content,
-      answer: step.answer
+      answer: convertAnswer(step.answer),
+      output: {
+        successMessage: step.stepSuccess
+      }
     };
   });
 
@@ -34,7 +44,7 @@ export const exportTutorial = async (
   const href = await URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.setAttribute("href", href);
-  link.setAttribute("download", "my_data.json");
+  link.setAttribute("download", `${tutName}.json`);
   // Apparently required to make it work on Firefox. Keeping this here
   // so we can maximize compatibility.
   document.body.appendChild(link);
